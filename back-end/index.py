34e0ddc202178499ccf360a4185a3683a1a9d1d7
Merry Cimakasky <mkcimakasky@gmail.com>
2019-07-16 14:57:17 -0700

    sketches

--- back-end/index.py
+++ back-end/index.py
@@ -2,6 +2,8 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from flask import Flask, jsonify, request
 from urllib.parse import urlparse, parse_qs
 from pipenv.vendor.dotenv import load_dotenv
+from models.location import Location
+from models.weather import Weather
 import os
 import json
 import requests 
@@ -10,51 +12,65 @@ load_dotenv()
 
 app = Flask(__name__)
 
-class SimpleHttpRequestHandler(BaseHTTPRequestHandler): 
-  def do_GET(self): 
+@app.route('/location') 
+def find_locations_data(): 
+  #need model 
+  #return jsonify([])
 
-    parsed_path = urlparse(self.path)
-    parsed_qs = parse_qs(parsed_path.query)
 
-    if parsed_path.path == '/locations' and parsed_qs.get('data'): 
+@app.route('/weather')
+def find_weathers_data(): 
+  #need model
+  #return josonify([])
 
-      api_key = os.getenv('GEOCODE_API_KEY')
+
+#-------------------------------------------------
+
+# class SimpleHttpRequestHandler(BaseHTTPRequestHandler): 
+#   def do_GET(self): 
+
+#     parsed_path = urlparse(self.path)
+#     parsed_qs = parse_qs(parsed_path.query)
+
+#     if parsed_path.path == '/locations' and parsed_qs.get('data'): 
+
+#       api_key = os.getenv('GEOCODE_API_KEY')
      
-      lat = parsed_qs['latitude'] 
-      long = parsed_qs['longitude']
+#       lat = parsed_qs['latitude'] 
+#       long = parsed_qs['longitude']
 
-      url = f'https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{long}&key={api_key}'
+#       url = f'https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{long}&key={api_key}'
 
-      result = requests.get(url).json()
+#       result = requests.get(url).json()
       
 
-    if parsed_path.path == '/weather': 
+#     if parsed_path.path == '/weather': 
 
-      api_key = os.getenv('WEATHER_API_KEY')
+#       api_key = os.getenv('WEATHER_API_KEY')
 
-      lat = parsed_qs['latitude'] 
-      long = parsed_qs['longitude'] 
+#       lat = parsed_qs['latitude'] 
+#       long = parsed_qs['longitude'] 
 
-      url = f'https://api.darksky.net/forecast/{api_key}/{lat},{long}'
+#       url = f'https://api.darksky.net/forecast/{api_key}/{lat},{long}'
       
-      result = requests.get(url).json() 
+#       result = requests.get(url).json() 
 
-def create_server(): 
-  PORT = os.getenv('PORT')
-  return HTTPServer(
-    ('127.0.0.1', PORT), SimpleHttpRequestHandler
-  )
+# def create_server(): 
+#   PORT = os.getenv('PORT')
+#   return HTTPServer(
+#     ('127.0.0.1', PORT), SimpleHttpRequestHandler
+#   )
 
-def run_forever(): 
-  PORT = os.getenv('PORT')
-  server = create_server()
+# def run_forever(): 
+#   PORT = os.getenv('PORT')
+#   server = create_server()
 
-  try: 
-    print(f'Starting up on port', PORT)
-    server.serve_forever() 
-  except KeyboardInterrupt: 
-    server.server_close()
-    server.shutdown() 
+#   try: 
+#     print(f'Starting up on port', PORT)
+#     server.serve_forever() 
+#   except KeyboardInterrupt: 
+#     server.server_close()
+#     server.shutdown() 
 
-if __name__ =='__main__': 
-  run_forever()      
+# if __name__ =='__main__': 
+#   run_forever()      
